ec9946c05a5b94ab50efc9aea4e6cdbf4642866b
Fastiraz <92016718+Fastiraz@users.noreply.github.com>
2025-10-12 19:44:53 +0200

    fix: unused import

--- src/akio/main.py
+++ src/akio/main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import asyncio
 import logging
 from .config.init import init
 init()
 from .lib.utils.env import create_vdb_if_needed
 from .lib.utils.cli import run_cli
-from .lib.tui.app import tui
 
 
 def main() -> None:
